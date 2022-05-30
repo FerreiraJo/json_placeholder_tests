b5452c972d7c569999b332da6510ce5c7e2f9ba4
FerreiraJo <jonathan_silva02@hotmail.com>
2022-05-29 23:50:07 -0400

    add spec configs

---
+++ spec/spec_helper.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+require 'httparty'
+require 'json-schema'
+
+RSpec.configure do |config|
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:all) do
+    class HttParty
+      include HTTParty
+      base_uri 'https://jsonplaceholder.typicode.com/'
+    end
+  end
+
+  RSpec::Matchers.define :match_response_schema do |schema|
+    match do |response|
+      schema_directory = "#{Dir.pwd}/spec/fixtures/schemas"
+      schema_path = "#{schema_directory}/#{schema}.json"
+      JSON::Validator.validate!(schema_path, response.body, strict: true)
+    end
+  end
+end
